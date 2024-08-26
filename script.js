ac44d46bdad9a3bd6ee8fc79b88d3d150825283f
Sandyie <you@example.com>
2024-08-26 16:49:51 -0300

    ATV3

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-function calc(id1, id2, respid, operador){
-    var num1 = parseFloat(document.getElementById(id1).value)
-    var num2 = parseFloat(document.getElementById(id2).value)
-    var resultado;
+function calcular(){
+    var batata1 = parseFloat(document.getElementById("num1").value)
+    var batata2 = parseFloat(document.getElementById("num2").value)
+    var operador = document.getElementById("operador").value
+    var resultado
 
     switch (operador) {
-    case "+":
-        resultado = num1 + num2
-        break;
-    case "-":
-        resultado = num1 - num2
-        break;
-    case "*"
-        resultado = num1 * num2
-        break; 
-    case "/"
-        if(num2 !== 0){
-            resultado = num1 / num2
-        }else{
-            resultado = "Erro: divisão por 0 não existe"
+        case "+":
+            resultado = batata1 + batata2
+            break;
+        case "-":
+            resultado = batata1 - batata2
+            break;
+        case "*":
+            resultado = batata1 * batata2
+            break;
+        case "/":
+            if(batata2 !== 0){
+            resultado = batata1 / batata2
+            }else{
+                resultado = "erro: divisão por 0 não existe!"
             }
         break;
-    default:
-        resultado = "Operador inválido"
+        default:
+            resultado = "Operador invalido"
             
     }
-    document.getElementById(respid).textContent = resultado
+    document.getElementById("resultado").textContent = resultado
+    
 }
