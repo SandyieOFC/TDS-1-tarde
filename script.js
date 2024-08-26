c88df51372bc6910310682e98c8e5eb22346e9e5
Sandyie <you@example.com>
2024-08-26 15:48:24 -0300

    atividade 2

---
+++ script.js
@@ -0,0 +1,28 @@
+function calc(id1, id2, respid, operador){
+    var num1 = parseFloat(document.getElementById(id1).value)
+    var num2 = parseFloat(document.getElementById(id2).value)
+    var resultado;
+
+    switch (operador) {
+    case "+":
+        resultado = num1 + num2
+        break;
+    case "-":
+        resultado = num1 - num2
+        break;
+    case "*"
+        resultado = num1 * num2
+        break; 
+    case "/"
+        if(num2 !== 0){
+            resultado = num1 / num2
+        }else{
+            resultado = "Erro: divisão por 0 não existe"
+            }
+        break;
+    default:
+        resultado = "Operador inválido"
+            
+    }
+    document.getElementById(respid).textContent = resultado
+}
